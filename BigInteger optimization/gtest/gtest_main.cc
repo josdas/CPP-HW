806f45ded5462c94ac64ca257f99ea850bcde205
josdas <josdas@mail.ru>
2017-05-20 19:07:16 +0300

    fix

--- BigInteger optimization/gtest/gtest_main.cc
+++ BigInteger optimization/gtest/gtest_main.cc
@@ -26,13 +26,13 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+//#include <vld.h>
 #include <stdio.h>
 
 #include <gtest/gtest.h>
 
 GTEST_API_ int main(int argc, char **argv) {
-	_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
   printf("Running main() from gtest_main.cc\n");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
