e785d083e09286626bfe2ffb8e5ddac7badd8a25
josdas <josdas@mail.ru>
2017-05-21 13:14:50 +0300

    cmake fix

--- Huffman/utility/lib_header/exceptions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <exception>
-
-struct Decoder_tree_error : public std::exception {
-	char const *what() const noexcept override {
-		return "Failed to decode huffman tree";
-	}
-};
